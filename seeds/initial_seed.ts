026cb34c296823889bc71221e7348edab39c3cb6
Felipe Buiras <f.morisan@gmail.com>
2026-09-10 19:47:41 -0300

    chore: add seed password hashes

--- seeds/initial_seed.ts
+++ seeds/initial_seed.ts
@@ -20,7 +20,7 @@ export async function seed(knex: Knex): Promise<void> {
             id: '9795f84a-6836-4e13-84e1-7a1db8bd24d0',
             username: 'gracegs',
             email: 'grace@gotham.studios',
-            password_hash: '',
+            password_hash: '34adfa7497e65a614fdbb3426c1e7bf9:f561973413dd6642be8650c3d50b979f96d2c9493ef8b95f77147217cd9578dd82e273970be487bd532b9774617731775c4e7e470133abf3548bd9143ca9a5b9',
             employer: '849a4821-fb9a-4c6a-8fff-662cc37f6802'
 
         },
@@ -28,7 +28,7 @@ export async function seed(knex: Knex): Promise<void> {
             id: '1f616ea9-176d-461c-b14c-65241f81e3f2',
             username: 'mark.evil',
             email: 'mark@evilrecords.com',
-            password_hash: '',
+            password_hash: 'f7c5590a19800d715da1d459b690926b:c63cec8cb78f5a09361e1e016ed69b5f5902d064c1dd524b880ec0de6824000665294faf42ab5aa2ad64a8f984f997e082114e4c8ed25fa8923dbad46b63dafe',
             employer: 'b03a0ef7-1ff8-43aa-9085-5c3f075e9666',
         }
     ])

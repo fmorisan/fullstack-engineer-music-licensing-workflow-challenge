be5cd353255807f519500cc5b8e50e4793b710dc
Felipe Buiras <f.morisan@gmail.com>
2026-09-10 21:22:44 -0300

    feat: add song creation endpoint

---
+++ api/songs.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import SongController from "../controllers/song";
+import { isLoggedIn, isUserType, validateRequestBody } from "../middlewares";
+
+const songs = Router()
+
+songs.post('/',
+    isLoggedIn,
+    isUserType('record_label'),
+    validateRequestBody(SongController.CreateSongSchema),
+    async (req, res) => {
+
+    const result = await SongController.createSong(req.auth!, req.body)
+
+    if (result.success) {
+        res.json(result.value)
+    } else {
+        res.status(400).json({error: result.error})
+    }
+})
+
+export default songs
---
+++ controllers/song.ts
@@ -0,0 +1,51 @@
+import z from "zod";
+import { AuthClaims } from "./login";
+import db from "../db";
+import { uuidv7 } from "uuidv7";
+
+const CreateSongSchema = z.object({
+    name: z.string(),
+    author: z.string(),
+    length_seconds: z.number().int().positive(),
+})
+
+type CreateSongData = z.infer<typeof CreateSongSchema>
+
+const createSong = async (user: AuthClaims, songData: CreateSongData) => {
+    const inserted = await db('songs').insert({
+        id: uuidv7() as any,
+        name: songData.name,
+        author: songData.author,
+        length_seconds: songData.length_seconds,
+        label_id: user.company_id as any
+    }).returning('*')
+
+    const song = inserted[0]
+
+    if (!song) {
+        return {
+            success: false,
+            error: 'could not create song'
+        }
+    }
+
+    // TODO: trigger indexing?
+
+    return {
+        success: true,
+        value: song
+    }
+}
+
+const uploadSongBoxArt = async (user: AuthClaims) => {
+    // TODO call S3 and get pre-signed URL
+}
+
+const SongController = {
+    CreateSongSchema,
+    createSong,
+    uploadSongBoxArt
+}
+
+export default SongController
+

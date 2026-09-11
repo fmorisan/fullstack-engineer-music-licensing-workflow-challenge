6659ae6cabb78437c75c3d0a0703f75dba8b7566
Felipe Buiras <f.morisan@gmail.com>
2026-09-11 10:05:23 -0300

    chore: use port env in server startup log

--- index.ts
+++ index.ts
@@ -2,4 +2,9 @@ import db from "./db"
 import express from "express"
 import app from "./api"
 
-app.listen(process.env.PORT ?? 8000, () => console.log('listening on 8000'))
+const PORT = process.env.PORT ?? 8000
+
+app.listen(
+    PORT,
+    () => console.log(`Music Licensing Service listening on :${PORT}`)
+)
